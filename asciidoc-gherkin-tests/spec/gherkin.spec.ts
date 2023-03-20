426947cfc6c734e2882545bf1701f3000e4629cf
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2023-03-20 15:30:05 +0100

    chore: added tests for scenario outlines

--- asciidoc-gherkin-tests/spec/gherkin.spec.ts
+++ asciidoc-gherkin-tests/spec/gherkin.spec.ts
@@ -1,6 +1,6 @@
 import expect from "expect";
 
-let cucumberRunner: any = undefined;
+let cucumberRunner: (arg: string) => Promise<Document> = undefined;
 
 describe('Gherkin With Asciidoc', () => {
     beforeEach(() => {
@@ -11,15 +11,31 @@ describe('Gherkin With Asciidoc', () => {
         jest.resetModules();
     })
 
-    it('Should insert results for Examples', async () => {
+    it('Inserts results on Rule / Example', async () => {
         let document = await cucumberRunner('spec/features/simple.feature.adoc');
 
         expect(document.getElementById('_example_simple_example').textContent).toContain('☑');
     });
 
-    it('Should insert results for Scenarios', async () => {
+    it('Insert results on Scenarios', async () => {
         let document = await cucumberRunner('spec/features/simple-scenario.feature.adoc');
 
         expect(document.getElementById('_scenario_simple_example').textContent).toContain('☑');
     });
+
+    it('Insert results on Steps', async () => {
+        let document = await cucumberRunner('spec/features/simple-scenario.feature.adoc');
+        let stepText = 'Given A';
+
+        const xpath = `//*[contains(text(),'${stepText}')]`;
+        const res = document.evaluate(xpath, document, null, 9);
+
+        expect(res.singleNodeValue.textContent).toContain('☑');
+    });
+
+    it('Has multiple results for scenario outlines', async () => {
+        let document = await cucumberRunner('spec/features/outline.feature.adoc');
+
+        expect(document.getElementById('_scenario_outline_eating').textContent).toContain('☑ ☑ ☑');
+    })
 });
--- asciidoc-gherkin-tests/spec/glue/glue.ts
+++ asciidoc-gherkin-tests/spec/glue/glue.ts
@@ -9,3 +9,15 @@ Given("A", function () {
 Given("B", function () {
     console.log("Executing B");
 })
+
+Given('there are {int} cucumbers', function (cucumbers: number) {
+
+});
+
+Given('I eat {int} cucumbers', function (cucumbers: number) {
+
+});
+
+Given('I should have {int} cucumbers', function (cucumbers: number) {
+
+});
